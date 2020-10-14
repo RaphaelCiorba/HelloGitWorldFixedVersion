22cb77f6e2528c1222f70817c47a72a59e438f02
Raphael <raphaeltobias478@gmail.com>
2020-10-14 21:54:58 +0200

    Added variables, scanner and simple text output

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int nummer1, nummer2, summe;
+
+        System.out.println("****************Sum of two numbers*****************");
+        System.out.println("Enter First Number: ");
+
+        System.out.println("Enter Second Number");
+
+        System.out.println("The sum of these numbers: " +summe);
 
     }
 }
