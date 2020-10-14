be26b78b7d9423855bace87e4312e6f4e39e15fd
Raphael Ciorba <raphaeltobias.ciorba@htl-wels.at>
2020-10-14 22:27:19 +0200

    Added gitignore and simple multiplication  of two numbers

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int nummer1, nummer2, summe;
+        int nummer1, nummer2, mult1, mult2, summe, multsum;
 
         try {
             System.out.println("****************Sum of two numbers*****************");
@@ -16,6 +16,16 @@ public class Main {
 
             summe = nummer1 + nummer2;
             System.out.println("The sum of these numbers: " +summe);
+
+            System.out.println("****************Multiplication of two numbers*****************");
+            System.out.println("Enter First Number: ");
+            mult1 = sc.nextInt();
+
+            System.out.println("Enter Second Number");
+            mult2 = sc.nextInt();
+
+            multsum = mult1 * mult2;
+            System.out.println("The product of these numbers: " +multsum);
         }
         catch (Exception e) {
             System.out.println("Error, please type in valid numbers");
